3744b6e2b21570cf8d1bff39a362bb201a72d2c4
Farhan Basheer <farhanfb6@outlook.com>
2021-08-06 17:33:52 +0500

    Secret Key is Safe

--- cleaning_calculator_webapp/settings.py
+++ cleaning_calculator_webapp/settings.py
@@ -19,8 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7hj*^@&9l_9$ou_z0pk&*^(v4*)=7%chot=4+l8ekhc_w5(w%i'
+
+with open('secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
