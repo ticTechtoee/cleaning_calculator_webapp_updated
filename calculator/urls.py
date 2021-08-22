1d3fd5f12af97cd9fcedd5dd9ecbdaa33676ff69
Farhan Basheer <farhanfb6@outlook.com>
2021-08-22 05:03:13 +0500

    Updated UI and Working.

--- calculator/urls.py
+++ calculator/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "calculator"
 
 urlpatterns = [
-    path('', views.index , name='index'),
+    #path('', views.index , name='index'),
+    path('', views.ui, name='ui'),
 ]
--- cleaning_calculator_webapp/static/assets/js/calculations.js
+++ cleaning_calculator_webapp/static/assets/js/calculations.js
@@ -1,211 +1,283 @@
-b3Value = function()
+
+
+range=function()
+ {
+
+            if(document.getElementById('a01').value >= 0 && document.getElementById('a01').value <= 1500)
+               {
+               var val = 75 * 0.5;
+               document.getElementById('a1').value = val;
+               }
+
+               if(document.getElementById('a01').value > 1500 && document.getElementById('a01').value <= 1750)
+               {
+               var val = 75 * 0.55;
+               document.getElementById('a1').value = val;
+               }
+
+               if(document.getElementById('a01').value > 1750 && document.getElementById('a01').value <= 2000)
+               {
+               var val = 75 * 0.6;
+               document.getElementById('a1').value = val;
+               }
+
+               if(document.getElementById('a01').value > 2000 && document.getElementById('a01').value <= 2250)
+               {
+               var val = 75 * 0.65;
+               document.getElementById('a1').value = val;
+               }
+
+               if(document.getElementById('a01').value > 2250 && document.getElementById('a01').value <= 2500)
+               {
+               var val = 75 * 0.7;
+               document.getElementById('a1').value = val;
+               }
+
+               if(document.getElementById('a01').value > 2500 && document.getElementById('a01').value <= 2750)
+               {
+               var val = 75 * 0.75;
+               document.getElementById('a1').value = val;
+               }
+
+               if(document.getElementById('a01').value > 2751 && document.getElementById('a01').value <= 3000)
+               {
+               var val = 75 * 0.8;
+               document.getElementById('a1').value = val;
+               }
+
+               if(document.getElementById('a01').value > 3000 && document.getElementById('a01').value <= 3250)
+               {
+               var val = 75 * 0.85;
+               document.getElementById('a1').value = val;
+               }
+
+               if(document.getElementById('a01').value > 3250 && document.getElementById('a01').value <= 3500)
+               {
+               var val = 75 * 1;
+               document.getElementById('a1').value = val;
+               }
+
+               if(document.getElementById('a01').value > 3500 && document.getElementById('a01').value <= 4000)
+               {
+               var val = 75 * 0.5;
+               document.getElementById('a1').value = val;
+               }
+
+               if(document.getElementById('a01').value > 4000 && document.getElementById('a01').value <= 4500)
+               {
+               var val = 75 * 1.5;
+               document.getElementById('a1').value = val;
+               }
+
+               if(document.getElementById('a01').value > 4500 && document.getElementById('a01').value <= 5000)
+               {
+               var val = 75 * 2;
+               document.getElementById('a1').value = val;
+               }
+
+               if(document.getElementById('a01').value > 5000 && document.getElementById('a01').value <= 5500)
+               {
+               var val = 75 * 2.5;
+               document.getElementById('a1').value = val;
+               }
+
+               if(document.getElementById('a01').value > 5500 && document.getElementById('a01').value <= 6000)
+               {
+               var val = 75 * 3;
+               document.getElementById('a1').value = val;
+               }
+               if(document.getElementById('a01').value > 6000 )
+               {
+                              document.getElementById('a1').value = 'Please Contact Us';
+               }
+        }
+
+AAValue = function()
 {
-var b1 = document.getElementById('b1').value;
-var rate1 = document.getElementById('rate1').value;
-document.getElementById('b3').value = parseInt(b1) * parseInt(rate1);
+var b1Value = document.getElementById('b1').value;
+var rate1Value = document.getElementById('rate1').value;
+document.getElementById('AA').value = parseFloat(b1Value) * parseFloat(rate1Value);
 }
 
-c3Value = function()
+ABValue = function()
 {
-var c1 = document.getElementById('c1').value;
-var rate2 = document.getElementById('rate2').value;
-document.getElementById('c3').value = parseInt(c1) * parseInt(rate2);
+var b1Value = document.getElementById('c1').value;
+var rate1Value = document.getElementById('rate2').value;
+document.getElementById('AB').value = parseFloat(b1Value) * parseFloat(rate1Value);
 }
 
-d3Value = function()
+ACValue = function()
 {
-var d1 = document.getElementById('d1').value;
-var rate3 = document.getElementById('rate3').value;
-document.getElementById('d3').value = parseInt(d1) * parseInt(rate3);
+var b1Value = document.getElementById('d1').value;
+var rate1Value = document.getElementById('rate3').value;
+document.getElementById('AC').value = parseFloat(b1Value) * parseFloat(rate1Value);
 }
 
-e3Value = function()
+ADValue = function()
 {
-var e1 = document.getElementById('e1').value;
-var rate4 = document.getElementById('rate4').value;
-document.getElementById('e3').value = parseInt(e1) * parseInt(rate4);
+var b1Value = document.getElementById('e1').value;
+var rate1Value = document.getElementById('rate4').value;
+document.getElementById('AD').value = parseFloat(b1Value) * parseFloat(rate1Value);
 }
 
-f3Value = function()
+AEValue = function()
 {
-var f1 = document.getElementById('f1').value;
-var rate5 = document.getElementById('rate5').value;
-document.getElementById('f3').value = parseInt(f1) * parseInt(rate5);
+var b1Value = document.getElementById('f1').value;
+var rate1Value = document.getElementById('rate5').value;
+document.getElementById('AE').value = parseFloat(b1Value) * parseFloat(rate1Value);
 }
 
-g3Value = function()
+AFValue = function()
 {
-var g1 = document.getElementById('g1').value;
-var rate6 = document.getElementById('rate6').value;
-document.getElementById('g3').value = parseInt(g1) * parseInt(rate6);
+var b1Value = document.getElementById('h1').value;
+var rate1Value = document.getElementById('rate6').value;
+document.getElementById('AF').value = parseFloat(b1Value) * parseFloat(rate1Value);
 }
 
-h3Value = function()
+AGValue = function()
 {
-var h1 = document.getElementById('h1').value;
-var rate7 = document.getElementById('rate7').value;
-document.getElementById('h3').value = parseInt(h1) * parseInt(rate7);
+var b1Value = document.getElementById('i1').value;
+var rate1Value = document.getElementById('rate7').value;
+document.getElementById('AG').value = parseFloat(b1Value) * parseFloat(rate1Value);
 }
 
-i3Value = function()
+AHValue = function()
 {
-var i1 = document.getElementById('i1').value;
-var rate8 = document.getElementById('rate8').value;
-document.getElementById('i3').value = parseInt(i1) * parseInt(rate8);
+var b1Value = document.getElementById('j1').value;
+var rate1Value = document.getElementById('rate8').value;
+document.getElementById('AH').value = parseFloat(b1Value) * parseFloat(rate1Value);
 }
 
-j3Value = function()
+AIValue = function()
 {
-var j1 = document.getElementById('j1').value;
-var rate9 = document.getElementById('rate9').value;
-document.getElementById('j3').value = parseInt(j1) * parseInt(rate9);
+var b1Value = document.getElementById('k1').value;
+var rate1Value = document.getElementById('rate9').value;
+document.getElementById('AI').value = parseFloat(b1Value) * parseFloat(rate1Value);
 }
 
-k3Value = function()
+AJValue = function()
 {
-var k1 = document.getElementById('k1').value;
-var rate10 = document.getElementById('rate10').value;
-document.getElementById('k3').value = parseInt(k1) * parseInt(rate10);
+var b1Value = document.getElementById('l1').value;
+var rate1Value = document.getElementById('rate10').value;
+document.getElementById('AJ').value = parseFloat(b1Value) * parseFloat(rate1Value);
 }
 
-l3Value = function()
+AKValue = function()
 {
-var l1 = document.getElementById('l1').value;
-var rate11 = document.getElementById('rate11').value;
-document.getElementById('l3').value = parseInt(l1) * parseInt(rate11);
+var b1Value = document.getElementById('m1').value;
+var rate1Value = document.getElementById('rate11').value;
+document.getElementById('AK').value = parseFloat(b1Value) * parseFloat(rate1Value);
 }
 
-m3Value = function()
+ALValue = function()
 {
-var m1 = document.getElementById('m1').value;
-var rate12 = document.getElementById('rate12').value;
-document.getElementById('m3').value = parseInt(m1) * parseInt(rate12);
+var b1Value = document.getElementById('n1').value;
+var rate1Value = document.getElementById('rate12').value;
+document.getElementById('AL').value = parseFloat(b1Value) * parseFloat(rate1Value);
 }
 
-n3Value = function()
+AMValue = function()
 {
-var n1 = document.getElementById('n1').value;
-var rate13 = document.getElementById('rate13').value;
-document.getElementById('n3').value = parseInt(n1) * parseInt(rate13);
+var b1Value = document.getElementById('o1').value;
+var rate1Value = document.getElementById('rate13').value;
+document.getElementById('AM').value = parseFloat(b1Value) * parseFloat(rate1Value);
 }
 
-o3Value = function()
+ANValue = function()
 {
-var o1 = document.getElementById('o1').value;
-var rate14 = document.getElementById('rate14').value;
-document.getElementById('o3').value = parseInt(o1) * parseInt(rate14);
+var b1Value = document.getElementById('o1').value;
+var rate1Value = document.getElementById('rate14').value;
+document.getElementById('AN').value = parseFloat(b1Value) * parseFloat(rate1Value);
 }
-var result;
 
 sumOfValues = function()
 {
-var a1 = document.getElementById('a1').value;
-var b3 = document.getElementById('b3').value;
-var c3 = document.getElementById('c3').value;
-var d3 = document.getElementById('d3').value;
-var e3 = document.getElementById('e3').value;
-var f3 = document.getElementById('f3').value;
-var g3 = document.getElementById('g3').value;
-var h3 = document.getElementById('h3').value;
-var i3 = document.getElementById('i3').value;
-var j3 = document.getElementById('j3').value;
-var k3 = document.getElementById('k3').value;
-var l3 = document.getElementById('l3').value;
-var m3 = document.getElementById('m3').value;
-var n3 = document.getElementById('n3').value;
-var o3 = document.getElementById('o3').value;
+
+var a1 = document.getElementById('AA').value;
+var b3 = document.getElementById('AB').value;
+var c3 = document.getElementById('AC').value;
+var d3 = document.getElementById('AD').value;
+var e3 = document.getElementById('AE').value;
+var f3 = document.getElementById('AF').value;
+var g3 = document.getElementById('AG').value;
+var h3 = document.getElementById('AH').value;
+var i3 = document.getElementById('AI').value;
+var j3 = document.getElementById('AJ').value;
+var k3 = document.getElementById('AK').value;
+var l3 = document.getElementById('AL').value;
+var m3 = document.getElementById('AM').value;
+var n3 = document.getElementById('AN').value;
+var o3 = document.getElementById('a1').value;
 
 var value1 = 15;
 var value2 = 30;
 
-result = parseInt(a1) + parseInt(b3) + parseInt(c3) + parseInt(d3) + parseInt(e3) + parseInt(f3) + parseInt(g3) + parseInt(h3) + parseInt(i3) + parseInt(j3) + parseInt(k3) + parseInt(l3) + parseInt(m3) + parseInt(n3) + parseInt(o3);
-document.getElementById('p1').value = result;
-document.getElementById('q1').value = result + parseInt(value1);
-document.getElementById('r1').value = result + parseInt(value2);
-
+var result = parseInt(a1) + parseInt(b3) + parseInt(c3) + parseInt(d3) + parseInt(e3) + parseInt(f3) + parseInt(g3) + parseInt(h3) + parseInt(i3) + parseInt(j3) + parseInt(k3) + parseInt(l3) + parseInt(m3) + parseInt(n3) + parseInt(o3);
+document.getElementById('q1').value = result;
+document.getElementById('r1').value = result + parseInt(value1);
+document.getElementById('s1').value = result + parseInt(value2);
 }
-//It's the value of p2
-p4Value = function()
+
+tax = function()
 {
-var p4 = document.getElementById('p4').value;
-document.getElementById('p2').value = (parseFloat(result) * (parseFloat(p4)/100)).toFixed(2);
+var tax_ratio = document.getElementById('tax_ratio').value;
+var q1Value = document.getElementById('q1').value;
+
+var r1Value = document.getElementById('r1').value;
+var s1Value = document.getElementById('s1').value;
+
+
+var result1 = q1Value * parseFloat(tax_ratio/100);
+var result2 = r1Value * parseFloat(tax_ratio/100);
+var result3 = s1Value * parseFloat(tax_ratio/100);
+
+document.getElementById('tax1').value = result1;
+document.getElementById('tax2').value = result2;
+document.getElementById('tax3').value = result3;
 }
 
-p3Value = function()
-{
-var p1 = document.getElementById('p1').value;
-var p2 = document.getElementById('p2').value;
-document.getElementById('p3').value = Math.round(parseFloat(p1) + parseFloat(p2));
-}
-
-q2Value = function()
-{
-var p4 = document.getElementById('p4').value;
-var q1 = document.getElementById('q1').value;
-document.getElementById('q2').value = (parseFloat(q1) * (parseFloat(p4)/100)).toFixed(2);
-}
-
-q3Value = function()
+total = function()
 {
 var q1 = document.getElementById('q1').value;
-var q2 = document.getElementById('q2').value;
-document.getElementById('q3').value = Math.round(parseFloat(q1) + parseFloat(q2));
-}
-
-r2Value = function()
-{
-var p4 = document.getElementById('p4').value;
 var r1 = document.getElementById('r1').value;
-document.getElementById('r2').value = (parseFloat(r1) * (parseFloat(p4)/100)).toFixed(2);
+var s1 = document.getElementById('s1').value;
+
+var tax1 =document.getElementById('tax1').value;
+var tax2 = document.getElementById('tax2').value;
+var tax3 = document.getElementById('tax3').value;
+
+var total1 = parseFloat(q1) + parseFloat(tax1);
+var total2 = parseFloat(r1) + parseFloat(tax2);
+var total3 = parseFloat(s1) + parseFloat(tax3);
+
+document.getElementById('t1').value = total1;
+document.getElementById('t2').value = total2;
+document.getElementById('t3').value = total3;
+
 }
 
-r3Value = function()
+clear = function()
 {
-var r1 = document.getElementById('r1').value;
-var r2 = document.getElementById('r2').value;
-document.getElementById('r3').value = Math.round(parseFloat(r1) + parseFloat(r2));
-}
+document.getElementById('a01').value = '';
+document.getElementById('b1').value = '';
+document.getElementById('c1').value = '';
+document.getElementById('d1').value = '';
+document.getElementById('e1').value = '';
+document.getElementById('f1').value = '';
+document.getElementById('h1').value = '';
+document.getElementById('i1').value = '';
+document.getElementById('j1').value = '';
+document.getElementById('k1').value = '';
+document.getElementById('l1').value = '';
+document.getElementById('m1').value = '';
+document.getElementById('n1').value = '';
+document.getElementById('o1').value = '';
+document.getElementById('p1').value = '';
 
-//Estimate for One-Time And Initial Clean
+document.getElementById('t1').value = '';
+document.getElementById('t2').value = '';
+document.getElementById('t3').value = '';
 
-s1Value = function()
-{
-var p3 = document.getElementById('p3').value;
-var multiple = parseFloat(1.2);
-document.getElementById('s1').innerHTML = Math.round(parseFloat(p3) * parseFloat(multiple));
-}
 
-s2Value = function()
-{
-var p3 = document.getElementById('p3').value;
-var multiple = parseFloat(1.5);
-document.getElementById('s2').innerHTML = Math.round(parseFloat(p3) * parseFloat(multiple));
-}
 
-t1Value = function()
-{
-var q3 = document.getElementById('q3').value;
-var multiple = parseFloat(1.2);
-document.getElementById('t1').innerHTML = Math.round(parseFloat(q3) * parseFloat(multiple));
-}
-
-t2Value = function()
-{
-var q3 = document.getElementById('q3').value;
-var multiple = parseFloat(1.5);
-document.getElementById('t2').innerHTML = Math.round(parseFloat(q3) * parseFloat(multiple));
-}
-
-u1Value = function()
-{
-var r3 = document.getElementById('r3').value;
-var multiple = parseFloat(1.2);
-document.getElementById('u1').innerHTML = Math.round(parseFloat(r3) * parseFloat(multiple));
-}
-
-u2Value = function()
-{
-var r3 = document.getElementById('r3').value;
-var multiple = parseFloat(1.5);
-document.getElementById('u2').innerHTML = Math.round(parseFloat(r3) * parseFloat(multiple));
 }
